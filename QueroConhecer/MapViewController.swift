bbc4b665789e7aa886d6995b810f6d5e323b443d
Gilmar Queiroz <gilmardequeiroz@hotmail.com>
2021-04-18 22:27:41 -0300

    Mkannotation ajuste de layout

--- QueroConhecer/MapViewController.swift
+++ QueroConhecer/MapViewController.swift
@@ -11,22 +11,56 @@ import MapKit
 
 class MapViewController: UIViewController {
 
+    //MARK: - Outlet
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var viInfo: UIView!
     @IBOutlet weak var lbName: UILabel!
     @IBOutlet weak var lbAddress: UILabel!
-    
+
+    //MARK: - variable
+    var places: [Place]!
+
+     //MARK: - life cyclo
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        for place in places {
+            addToMap(place)
+        }
+        showPlaces()
+        setupView()
     }
-    
+
+     //MARK: - methods
+    func addToMap(_ place:Place) {
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = place.coordinate
+        annotation.title = place.name
+        mapView.addAnnotation(annotation)
+    }
+
+    func showPlaces() {
+        mapView.showAnnotations(mapView.annotations, animated: true)
+    }
+
+    func setupView() {
+        self.searchBar.isHidden = true
+        self.viInfo.isHidden = true
+
+        if places.count == 1 {
+            title = places[0].name
+        } else {
+            title = "Meus lugares"
+        }
+    }
+
+    //MARK: = action
     @IBAction func showRoute(_ sender: Any) {
     }
     
     @IBAction func showSearchBar(_ sender: Any) {
     }
+
+
     
 }
--- QueroConhecer/PlacesTableViewController.swift
+++ QueroConhecer/PlacesTableViewController.swift
@@ -13,22 +13,43 @@ class PlacesTableViewController: UITableViewController {
     //MARK: - variable
     var places: [Place] = []
     let userDefaults = UserDefaults.standard
+    var lbNoPlaces: UILabel!
 
     //MARK: - life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         loadPlaces()
+        noplaceButton()
+        savePlaces()
     }
     //MARK: - segue
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier! != "mapSegue" {
             let vc = segue.destination as! PlaceFinderViewController
             vc.delegate = self
+        } else {
+            let vc = segue.destination as! MapViewController
+            switch sender {
+                case let place as Place:
+                    vc.places = [place]
+                default:
+                    vc.places = places 
+            }
+
         }
     }
 
     // MARK: - Table view data source
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if places.count > 0 {
+            let btShowAll = UIBarButtonItem(title: "Mostra todos no mapa", style:.plain, target: self, action: #selector(showAll))
+            navigationItem.leftBarButtonItem = btShowAll
+            tableView.backgroundView = nil
+        } else {
+            navigationItem.leftBarButtonItem = nil
+            tableView.backgroundView = lbNoPlaces
+        }
+
         return places.count
     }
 
@@ -39,10 +60,22 @@ class PlacesTableViewController: UITableViewController {
         return cell
     }
 
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let place = places[indexPath.row]
+        performSegue(withIdentifier: "mapSegue", sender: place)
+    }
+
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle  == .delete {
+            places.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+            savePlaces()
+        }
+    }
 
     //MARK: - methods
     func loadPlaces() {
-        if let placesData = userDefaults.data(forKey: "plaves") {
+        if let placesData = userDefaults.data(forKey: "places") {
             do{
                 places = try JSONDecoder().decode([Place].self, from: placesData)
                 tableView.reloadData()
@@ -52,9 +85,21 @@ class PlacesTableViewController: UITableViewController {
         }
     }
 
-    func  savePlaces() {
+    func savePlaces() {
         let json = try? JSONEncoder().encode(places)
         userDefaults.set(json, forKey: "places")
+    }
+
+    @objc func showAll() {
+         performSegue(withIdentifier: "mapSegue", sender: nil)
+    }
+
+    func noplaceButton() {
+        self.lbNoPlaces = UILabel()
+        self.lbNoPlaces.text = "Cadatre os locais que deseja conhecer\nclicador no botão + a cima"
+        self.lbNoPlaces.textAlignment = .center
+        self.lbNoPlaces.numberOfLines = 0
+
     }
 }
 
